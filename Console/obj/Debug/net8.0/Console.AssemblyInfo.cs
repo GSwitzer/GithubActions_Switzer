824a05332f24840a3ee981d5fe2eace700d65e3a
GSwitzer <gswitzer2@huskers.unl.edu>
2025-03-28 11:18:13 -0500

    Set up GitHub Actions to run unit tests

--- Console/obj/Debug/net8.0/Console.AssemblyInfo.cs
+++ Console/obj/Debug/net8.0/Console.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f61ee5a533ab8337e9905022e8a777d6f80c9f20")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8985834f61549a1234e1c17567506250b89232e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
